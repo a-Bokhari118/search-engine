d98493b8e4ea6cc38a75464a853a0097beca2850
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-10-25 19:06:26 +0300

    videos

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -81,7 +81,20 @@ const Results = () => {
         </div>
       );
     case '/videos':
-      return 'NEWS';
+      return (
+        <div className="flex flex-wrap">
+          {results.map((video, index) => (
+            <div key={index} className="p-2">
+              <ReactPlayer
+                url={video.additional_links[0].href}
+                controls
+                width="355px"
+                height="200px"
+              />
+            </div>
+          ))}
+        </div>
+      );
     default:
       return 'ERROR!';
   }
