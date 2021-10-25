76b7cedab1eaf5cb5d66c4c546b70f14fa07d59c
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-10-25 17:09:32 +0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,23 @@
-const Navbar = () => {
+import { Link } from 'react-router-dom';
+import Search from './Search';
+const Navbar = ({ darkTheme, setDarkTheme }) => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="p-4 flex flex-wrap pb-0 sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
+      <div className="flex justify-between items-center space-x-5 w-screen">
+        <Link to="/">
+          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+            🔥LAVA🔥
+          </p>
+        </Link>
+        <button
+          type="button"
+          onClick={() => setDarkTheme(!darkTheme)}
+          className="text-2xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-1 py-1 hover:shadow-lg"
+        >
+          {darkTheme ? '☀' : '🌑'}
+        </button>
+      </div>
+      <Search />
     </div>
   );
 };
